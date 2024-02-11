0ba7b6819a8542d7bff98bd39fbc398e97ccbbd0
wrteam-aayush <wrteam.aayush@gmail.com>
2024-02-11 16:32:02 +0530

    Added: All code

---
+++ index.js
@@ -0,0 +1,79 @@
+
+
+let port = process.env.PORT || 5000;
+
+let IO = require("socket.io")(port, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+IO.use((socket, next) => {
+  if (socket.handshake.query) {
+    let callerId = socket.handshake.query.callerId;
+    socket.user = callerId;
+    next();
+  } else {
+    // Handle the case where no query parameters are provided
+    next(new Error('No callerInfo provided'));
+  }
+});
+
+IO.on("connection", (socket) => {
+  console.log(socket.user, "Connected");
+  socket.join(socket.user);
+
+  socket.on("makeCall", (data) => {
+    let calleeId = data.calleeId.toString();
+    let callerInfo = data.callerInfo;
+    let sdpOffer = data.sdpOffer;
+
+    console.log(socket.user, " is CALLING ", calleeId);
+
+    socket.to(calleeId).emit("newCall", {
+      callerInfo: callerInfo,
+      sdpOffer: sdpOffer,
+      callId: data.callId,
+      callBackRequestId: data.callBackRequestId,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    let callerId = data.callerId.toString();
+    let sdpAnswer = data.sdpAnswer;
+
+    console.log(socket.userId, " is ANSWERING CALL FOR ", callerId);
+
+    socket.to(callerId).emit("callAnswered", {
+      callee: socket.user,
+      sdpAnswer: sdpAnswer,
+    });
+  });
+
+  socket.on("declineCall", (data) => {
+    let callerId = data.callerId.toString();
+    let reasonSlug = data.reasonSlug;
+
+    console.log(socket.user, " is ANSWERING CALL FOR ", callerId);
+
+    socket.to(callerId).emit("callDeclined", {
+      callee: socket.user,
+      reason: reasonSlug,
+    });
+  });
+
+  socket.on("IceCandidate", (data) => {
+    let calleeId = data.calleeId.toString();
+    let iceCandidate = data.iceCandidate;
+
+    console.log(socket.user, " is SENDING ICE CANDIDATE TO ", calleeId);
+
+    socket.to(calleeId).emit("IceCandidate", {
+      sender: socket.user,
+      iceCandidate: iceCandidate,
+    });
+  });
+});
+
+
